0e228397a10d8f0227596c360d6bab6aa008ab40
Nvlahogiannis12 <526Nvlahogiannis@frhsd.com>
2025-03-13 15:49:52 +0000

    Arrow key options works

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-/* 
-array = [item,item,item,item]
+//Select Player Action Option
+let playerActionsOptions = ['Attack', 'Shop', 'Skip']
 let index = 0
-right arrow index++
-left arrow index--
-*/
+
+window.addEventListener('keydown', (event) => {
+    if(event.key === "ArrowRight" || event.key === "ArrowDown"){
+        index++
+        if (index > playerActionsOptions.length - 1) index = 0;
+    //alert(playerActionsOptions[index])
+    }
+else if (event.key === "ArrowLeft" || event.key === "ArrowUp"){
+    index--
+    if (index < 0) index = playerActionsOptions.length - 1;
+    //alert(playerActionsOptions[index])
+}
+})
+document.getElementById('firstOptionButton').innerHTML = playerActionsOptions[0]
+document.getElementById('secondOptionButton').innerHTML = playerActionsOptions[1]
+document.getElementById('thirdOptionButton').innerHTML = playerActionsOptions[2]
