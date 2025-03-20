5312d1a5e97f4863a4cbb23d8f75d6417faa23f7
Nvlahogiannis12 <526Nvlahogiannis@frhsd.com>
2025-03-20 12:09:25 +0000

    button toggle

--- script.js
+++ script.js
@@ -2,76 +2,73 @@
 let playerTurn = true
 let playerActionsOptions = ["Attack", "Damage", "Option",];
 let index = 0;
+let playerHP = 100;
 isPlayerTurn()
+updatePlayerActionOption()
 
-function isPlayerTurn(){
-if(playerTurn === true){
-window.addEventListener("keydown", (event) => {
+function playerKeyDowns(event){
+  if(playerTurn === false) return;
   if (event.key === "ArrowRight" || event.key === "ArrowDown") {
     index++;
+    updatePlayerActionOption()
     if (index > playerActionsOptions.length - 1) index = 0;
-    for (let i = 0; i < playerActionsOptions.length; i++) {
-      document.getElementsByClassName("selectionBox")[i].textContent =
-        playerActionsOptions[i];
-    }
-    document.getElementsByClassName("selectionBox")[index].textContent =
-      "> " + document.getElementsByClassName("selectionBox")[index].textContent;
-  } else if (event.key === "ArrowLeft" || event.key === "ArrowUp") {
-    index--;
-    if (index < 0) index = playerActionsOptions.length - 1;
-    for (let i = 0; i < playerActionsOptions.length; i++) {
-      document.getElementsByClassName("selectionBox")[i].textContent =
-        playerActionsOptions[i];
-    }
-    document.getElementsByClassName("selectionBox")[index].textContent =
-      "> " + document.getElementsByClassName("selectionBox")[index].textContent;
+} else if (event.key === "ArrowLeft" || event.key === "ArrowUp") {
+  index--;
+  updatePlayerActionOption()
+  if (index < 0) index = playerActionsOptions.length - 1;
+} else if (event.key === 'Enter'){
+  playerActionOptionSelected()
+}
+}
+
+function updatePlayerActionOption(){
+  for (let i = 0; i < playerActionsOptions.length; i++) {
+    document.getElementsByClassName("selectionBox")[i].textContent =
+      playerActionsOptions[i];
   }
-});
-document.getElementById("firstOptionButton").innerHTML =
-  "> " + playerActionsOptions[0];
-document.getElementById("secondOptionButton").innerHTML =
-  playerActionsOptions[1];
-document.getElementById("thirdOptionButton").innerHTML =
-  playerActionsOptions[2];
+  document.getElementsByClassName("selectionBox")[index].textContent =
+      "> " + document.getElementsByClassName("selectionBox")[index].textContent;
+}
+
+function isPlayerTurn(){
+  if (playerTurn === true){
+window.addEventListener("keydown", playerKeyDowns);
+  } else{
+window.removeEventListener("keydown", playerKeyDowns);
+  }
+}
 
 
 
 //ENTER KEY EVENT LISTENER & Move Output
-let playerBattleOptionMenu = true;
-window.addEventListener("keydown", (event) => {
-  if (event.key === "Enter") {
-    if (playerBattleOptionMenu === true) {
-      if (index === 0) {
-        alert("one");
-      } else if (index === 1) {
-        if(playerHP <= 0){
-          //return stops function
-          playerHP = 0;
-          return;
-        }
-        playerHP -= 10;
-        gameWin();
-        alert(playerHP);
-      } else if (index === 2) {
-        playerHP = 0;
-        gameWin();
-      }
-    }
+function playerActionOptionSelected(){
+if (playerTurn === false) return;
+if (index === 0) {
+  alert("one");
+} else if (index === 1) {
+  if(playerHP <= 0){
+    //return stops function
+    playerHP = 0;
+    return;
   }
-});
+  playerHP -= 10;
+  gameWin();
+  alert(playerHP);
+} else if (index === 2) {
+  playerHP = 0;
+  gameWin();
+}
+}
+
+     
 //  GETS BUTTON OPTIONS TO STOP FUNCTIONING!!!
 //function words(){
 //    playerBattleOptionMenu = false
 //}
-} else if(playerTurn === false){
-  alert('working...')
-}
-}
 
 
 
 //HEALTH SYSTEMS
-let playerHP = 100;
 
 function gameWin() {
   let healthbar = document.getElementById("playerHealth");
@@ -84,7 +81,6 @@ function gameWin() {
 
 
 //opponent turn stuff
-
 /*
 function(){
 disable player stuff
@@ -105,6 +101,6 @@ window.addEventListener("keydown", (event) => {
 })
 
 function debugMode(){
-  playerTurn = false
+  playerTurn = false;
   isPlayerTurn()
 }
